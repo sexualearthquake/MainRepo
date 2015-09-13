3a74acdb0db81232d2e97beb672c69ab115a567a
sexualearthquake <dcgradus@gmail.com>
2015-09-13 12:45:59 +0300

    Змінив код на суму від 3 до 9.

--- src/com/sexualearthquake/ds/lab1/HomeWork1.java
+++ src/com/sexualearthquake/ds/lab1/HomeWork1.java
@@ -4,7 +4,7 @@ package com.sexualearthquake.ds.lab1;
 public class HomeWork1 {
 
     public static void main(String[] args) {
-        System.out.println(1 + 2 + 3 + 4 + 5);
+        System.out.println(3 + 4 + 5 + 6 + 7 + 8 + 9);
     }
 
 }
