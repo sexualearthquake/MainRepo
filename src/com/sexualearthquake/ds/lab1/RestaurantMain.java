2ffdbbefd3ab23f9abbe925cfcd5de73e8958a4c
sexualearthquake <dcgradus@gmail.com>
2015-10-07 23:01:48 +0300

    Trying to do something[2]

---
+++ src/com/sexualearthquake/ds/lab1/RestaurantMain.java
@@ -0,0 +1,20 @@
+package com.sexualearthquake.ds.lab1;
+
+/**
+ * Created by Daniel @dukerozen on 07.10.2015.
+ */
+public class RestaurantMain {
+
+    public static void main(String[] args) {
+
+        Dish[] dishes = new Dish[5];
+        dishes[0] = new Dish(5, "Pizza", 272);
+        dishes[1] = new Dish(4, "Soup", 131);
+        dishes[2] = new Dish(3, "Cheeseburger", 404);
+        dishes[3] = new Dish(6, "Spaghetti", 364);
+        dishes[4] = new Dish(4, "Hot-Dog", 388);
+
+        for (int i=0; int < dishes.price; i++);
+            System.out.println(dishes[i]);
+    }
+}
