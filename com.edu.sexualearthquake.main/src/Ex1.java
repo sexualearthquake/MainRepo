ecbecc434f84fc373d0e9b61a5809971541385cc
sexualearthquake <dcgradus@gmail.com>
2015-10-24 19:07:00 +0300

    Додав код гри на вгадування числа. Завдання на модуль номер 1.

---
+++ com.edu.sexualearthquake.main/src/Ex1.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by D on 24.10.2015.
+ */
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = new Random().nextInt(100);
+
+        int response;
+
+        System.out.println("Гра - відгадай число від 0 до 100!");
+
+        while (true) {
+            response = Integer.parseInt(scanner.nextLine());
+
+            if (response == number) {
+                System.out.println("Ви вгадали!");
+                break;
+            } else if (response < number) {
+                System.out.println("Число менше");
+            } else {
+                System.out.println("Число більше");
+            }
+        }
+
+    }
+}
+
