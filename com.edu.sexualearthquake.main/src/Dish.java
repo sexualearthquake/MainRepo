d740049866a4a281dd3358d8c8d0b9c536b5271e
sexualearthquake <dcgradus@gmail.com>
2015-10-24 21:47:07 +0300

    Клас Dish для задачі про столову

---
+++ com.edu.sexualearthquake.main/src/Dish.java
@@ -0,0 +1,23 @@
+/**
+ * Created by D on 24.10.2015.
+ */
+public class Dish {
+    int price;
+    int calories;
+    String name;
+
+    public Dish(int price, int calories, String name) {
+        this.price = price;
+        this.calories = calories;
+
+        this.name = name;
+    }
+
+    public String toString() {
+        return String.format(
+                " %9s страва\t, Цін -: %5d,\t Калорійність %d ",
+                this.price, this.name, this.calories,
+                );
+    }
+
+}
